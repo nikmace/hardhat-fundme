71c051c1ced0d91be74df3d5b829a55da7d34ee6
nikmace <nikmacentosh@gmail.com>
2022-06-21 16:38:13 +0300

    Deploy scripts for testing on localy/hardhat or on rinkeby testnet

--- deploy/00-deploy-mocks.ts
+++ deploy/00-deploy-mocks.ts
@@ -1,34 +1,32 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types"
-import { DeployFunction } from "hardhat-deploy/types"
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
 
-const DECIMALS = "18"
-const INITIAL_PRICE = "2000000000000000000000" // 2000
-const deployMocks: DeployFunction = async function (
+import {
+  developmentChains,
+  DECIMALS,
+  INITIAL_ANSWER,
+} from '../helper-hardhat-config';
+
+const deployMocks: DeployFunction = async function deployFunc(
   hre: HardhatRuntimeEnvironment
 ) {
-  const { deployments, getNamedAccounts, network } = hre
-  const { deploy, log } = deployments
-  const { deployer } = await getNamedAccounts()
-  const chainId = network.config.chainId
-  // If we are on a local development network, we need to deploy mocks!
-  if (chainId == 31337) {
-    log("Local network detected! Deploying mocks...")
-    await deploy("MockV3Aggregator", {
-      contract: "MockV3Aggregator",
+  const { deployments, getNamedAccounts, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  if (developmentChains.includes(network.name)) {
+    log('Local network detected! Deploying mocks...');
+    await deploy('MockV3Aggregator', {
+      contract: 'MockV3Aggregator',
       from: deployer,
       log: true,
-      args: [DECIMALS, INITIAL_PRICE],
-    })
-    log("Mocks Deployed!")
-    log("----------------------------------")
-    log(
-      "You are deploying to a local network, you'll need a local network running to interact"
-    )
-    log(
-      "Please run `yarn hardhat console` to interact with the deployed smart contracts!"
-    )
-    log("----------------------------------")
+      args: [DECIMALS, INITIAL_ANSWER],
+    });
+    log('Mocks deployed!');
+    log('--------------------------------------------');
   }
-}
-export default deployMocks
-deployMocks.tags = ["all", "mocks"]
+};
+
+export default deployMocks;
+deployMocks.tags = ['all', 'mocks'];
--- deploy/01-deploy-fund-me.ts
+++ deploy/01-deploy-fund-me.ts
@@ -1,40 +1,50 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types"
-import { DeployFunction } from "hardhat-deploy/types"
-import verify from "../utils/verify"
-import { networkConfig, developmentChains } from "../helper-hardhat-config"
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+
+import { developmentChains, networkConfig } from '../helper-hardhat-config';
+import verify from '../utils/verify';
 
 const deployFundMe: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
 ) {
-  // @ts-ignore
-  const { getNamedAccounts, deployments, network } = hre
-  const { deploy, log } = deployments
-  const { deployer } = await getNamedAccounts()
-  const chainId: number = network.config.chainId!
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId: number = network.config.chainId!;
 
-  let ethUsdPriceFeedAddress: string
-  if (chainId == 31337) {
-    const ethUsdAggregator = await deployments.get("MockV3Aggregator")
-    ethUsdPriceFeedAddress = ethUsdAggregator.address
+  //   const ethUsdPriceFeedAddress = networkConfig[chainId]['ethUsdPriceFeed'];
+  let ethUsdPriceFeedAddress: string;
+  if (developmentChains.includes(network.name)) {
+    const ethUsdAgregator = await deployments.get('MockV3Aggregator');
+    ethUsdPriceFeedAddress = ethUsdAgregator.address;
   } else {
-    ethUsdPriceFeedAddress = networkConfig[network.name].ethUsdPriceFeed!
+    ethUsdPriceFeedAddress = networkConfig[network.name].ethUsdPriceFeed!;
   }
-  log("----------------------------------------------------")
-  log("Deploying FundMe and waiting for confirmations...")
-  const fundMe = await deploy("FundMe", {
+
+  log('----------------------------------------------------');
+  log('Deploying FundMe and waiting for confirmations...');
+  console.log(`Deployer: ${deployer}`);
+
+  // When going for localhost or hardhat we want to use a mock
+  const args = [ethUsdPriceFeedAddress];
+  const fundMe = await deploy('FundMe', {
     from: deployer,
-    args: [ethUsdPriceFeedAddress],
+    args, // put priceFeed address
     log: true,
-    // we need to wait if on a live network so we can verify properly
-    waitConfirmations: networkConfig[network.name].blockConfirmations || 0,
-  })
-  log(`FundMe deployed at ${fundMe.address}`)
+    waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
+  });
+
+  log(`FundMe deployed at ${fundMe.address}`);
+
   if (
     !developmentChains.includes(network.name) &&
     process.env.ETHERSCAN_API_KEY
   ) {
-    await verify(fundMe.address, [ethUsdPriceFeedAddress])
+    // Verify
+    await verify(fundMe.address, args);
   }
-}
-export default deployFundMe
-deployFundMe.tags = ["all", "fundMe"]
+  log('--------------------------------------------');
+};
+
+export default deployFundMe;
+deployFundMe.tags = ['all', 'FundMe'];
