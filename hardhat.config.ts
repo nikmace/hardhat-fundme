427e49c8cafe40062d5f0fc5f3c09654cd420156
nikmace <nikmacentosh@gmail.com>
2022-06-21 16:41:21 +0300

    Hardhat configuration script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,13 @@
-import "@typechain/hardhat"
-import "@nomiclabs/hardhat-waffle"
-import "@nomiclabs/hardhat-etherscan"
-import "@nomiclabs/hardhat-ethers"
-import "hardhat-gas-reporter"
-import "dotenv/config"
-import "solidity-coverage"
-import "hardhat-deploy"
-import "solidity-coverage"
-import { HardhatUserConfig } from "hardhat/config"
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-gas-reporter';
+import 'dotenv/config';
+import 'solidity-coverage';
+import 'hardhat-deploy';
+import 'solidity-coverage';
+import { HardhatUserConfig } from 'hardhat/config';
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -15,35 +15,35 @@ import { HardhatUserConfig } from "hardhat/config"
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const KOVAN_RPC_URL =
-  process.env.KOVAN_RPC_URL ||
-  "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const PRIVATE_KEY =
-  process.env.PRIVATE_KEY ||
-  "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || '';
+const RINKEBY_RPC_URL =
+  process.env.RINKEBY_RPC_URL ||
+  'https://eth-mainnet.alchemyapi.io/v2/your-api-key';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
       chainId: 31337,
       // gasPrice: 130000000000,
     },
-    kovan: {
-      url: KOVAN_RPC_URL,
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
       accounts: [PRIVATE_KEY],
-      chainId: 42,
+      chainId: 4,
+      // @ts-ignore
+      blockConfirmations: 6,
     },
   },
   solidity: {
     compilers: [
       {
-        version: "0.8.8",
+        version: '0.8.8',
       },
       {
-        version: "0.6.6",
+        version: '0.6.6',
       },
     ],
   },
@@ -52,10 +52,10 @@ const config: HardhatUserConfig = {
   },
   gasReporter: {
     enabled: true,
-    currency: "USD",
-    outputFile: "gas-report.txt",
+    currency: 'USD',
+    outputFile: 'gas-report.txt',
     noColors: true,
-    // coinmarketcap: COINMARKETCAP_API_KEY,
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
   namedAccounts: {
     deployer: {
@@ -63,6 +63,6 @@ const config: HardhatUserConfig = {
       1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
     },
   },
-}
+};
 
-export default config
+export default config;
