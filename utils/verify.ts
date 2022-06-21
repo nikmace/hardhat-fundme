a6e742e243bf611eb7606fd5ac78982754c2b7a6
nikmace <nikmacentosh@gmail.com>
2022-06-21 16:39:24 +0300

    Script that verifies our smart contract

--- utils/verify.ts
+++ utils/verify.ts
@@ -1,19 +1,19 @@
-import { run } from "hardhat"
+import { run } from 'hardhat';
 
 const verify = async (contractAddress: string, args: any[]) => {
-  console.log("Verifying contract...")
+  console.log('Verifying contract...');
   try {
-    await run("verify:verify", {
+    await run('verify:verify', {
       address: contractAddress,
       constructorArguments: args,
-    })
+    });
   } catch (e: any) {
-    if (e.message.toLowerCase().includes("already verified")) {
-      console.log("Already verified!")
+    if (e.message.toLowerCase().includes('already verified')) {
+      console.log('Already verified!');
     } else {
-      console.log(e)
+      console.log(e);
     }
   }
-}
+};
 
-export default verify
+export default verify;
