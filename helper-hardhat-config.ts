5d8a70abef282acd1738146d19f88cd21fa40d10
nikmace <nikmacentosh@gmail.com>
2022-06-21 16:38:57 +0300

    Network config with price feeds for different testnets

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,10 +1,10 @@
 export interface networkConfigItem {
-  ethUsdPriceFeed?: string
-  blockConfirmations?: number
+  ethUsdPriceFeed?: string;
+  blockConfirmations?: number;
 }
 
 export interface networkConfigInfo {
-  [key: string]: networkConfigItem
+  [key: string]: networkConfigItem;
 }
 
 export const networkConfig: networkConfigInfo = {
@@ -12,10 +12,12 @@ export const networkConfig: networkConfigInfo = {
   hardhat: {},
   // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
   // Default one is ETH/USD contract on Kovan
-  kovan: {
-    ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+  rinkeby: {
+    ethUsdPriceFeed: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
     blockConfirmations: 6,
   },
-}
+};
 
-export const developmentChains = ["hardhat", "localhost"]
+export const developmentChains = ['hardhat', 'localhost'];
+export const DECIMALS = 8;
+export const INITIAL_ANSWER = 200000000000;
