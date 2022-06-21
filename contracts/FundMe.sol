b44f9022ec79f3fbd1cf50f3dc3025bbb720ba6c
nikmace <nikmacentosh@gmail.com>
2022-06-21 16:37:36 +0300

    Optimized contracts, added modificators & pure getter functions

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,42 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "./PriceConverter.sol";
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+import './PriceConverter.sol';
 
+/**
+  @title A contract for crowd funding
+  @author nkmcntsh
+  @notice This contract is to demo samle crowd funding contract
+  @dev This implements price feeds as our library
+ */
 contract FundMe {
   using PriceConverter for uint256;
 
-  mapping(address => uint256) public s_addressToAmountFunded;
-  address[] public s_funders;
-  address public s_owner;
+  mapping(address => uint256) private s_addressToAmountFunded;
+  address[] private s_funders;
+  address private immutable i_owner;
   AggregatorV3Interface public s_priceFeed;
+
+  modifier onlyOwner() {
+    require(msg.sender == i_owner);
+    _;
+  }
 
   constructor(address priceFeed) {
     s_priceFeed = AggregatorV3Interface(priceFeed);
-    s_owner = msg.sender;
+    i_owner = msg.sender;
   }
 
+  receive() external payable {
+    fund();
+  }
+
+  fallback() external payable {
+    fund();
+  }
+
+  /**
+  @notice This function funds this contract, and keeps track of funders & the amount they sent
+  @dev This implements price feeds as our library
+ */
   function fund() public payable {
     uint256 minimumUSD = 50 * 10**18;
     require(
       msg.value.getConversionRate(s_priceFeed) >= minimumUSD,
-      "You need to spend more ETH!"
+      'You need to spend more ETH!'
     );
     // require(PriceConverter.getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
     s_addressToAmountFunded[msg.sender] += msg.value;
     s_funders.push(msg.sender);
   }
 
-  function getVersion() public view returns (uint256) {
-    return s_priceFeed.version();
-  }
-
-  modifier onlyOwner() {
-    require(msg.sender == s_owner);
-    _;
-  }
-
+  /**
+  @notice This function withdraws funds to owner of this contract
+  @dev This implements price feeds as our library
+ */
   function withdraw() public payable onlyOwner {
     payable(msg.sender).transfer(address(this).balance);
     for (
@@ -48,16 +66,46 @@ contract FundMe {
       s_addressToAmountFunded[funder] = 0;
     }
     s_funders = new address[](0);
+
+    (bool success, ) = payable(msg.sender).call{ value: address(this).balance }(
+      ''
+    );
+
+    require(success, 'Transfer failed');
   }
 
   function cheaperWithdraw() public payable onlyOwner {
-    payable(msg.sender).transfer(address(this).balance);
+    // Copy s_funders array to memory, because it is cheaper
     address[] memory funders = s_funders;
-    // mappings can't be in memory, sorry!
-    for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-      address funder = funders[funderIndex];
+    for (uint256 funderIdx = 0; funderIdx < funders.length; funderIdx++) {
+      address funder = funders[funderIdx];
       s_addressToAmountFunded[funder] = 0;
     }
     s_funders = new address[](0);
+
+    (bool success, ) = i_owner.call{ value: address(this).balance }('');
+
+    require(success, 'Transfer failed');
+  }
+
+  // View / Pure functions
+  function getOwner() public view returns (address) {
+    return i_owner;
+  }
+
+  function getFunder(uint256 index) public view returns (address) {
+    return s_funders[index];
+  }
+
+  function getAddressToAmountFunded(address funder)
+    public
+    view
+    returns (uint256)
+  {
+    return s_addressToAmountFunded[funder];
+  }
+
+  function getPriceFeed() public view returns (AggregatorV3Interface) {
+    return s_priceFeed;
   }
 }
--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
 
 library PriceConverter {
   function getPrice(AggregatorV3Interface priceFeed)
